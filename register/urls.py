81607c586d653138cdd35fd05de01262f0a2a7ff
GaitanJulian <juliangaitan_h@hotmail.com>
2023-02-13 15:26:27 -0500

    add login function

--- register/urls.py
+++ register/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from . import views
-from .views import signup
+from .views import signup, signin
 
 urlpatterns = [
     #ruta, vista, nombre interno
-    path('signup/', signup, name='singup')
+    path('signup/', signup, name='singup'),
+    path('signin/', signin, name='singin'),
+
 ]
--- register/views.py
+++ register/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, authenticate
 # Create your views here.
 def signup(request):
     context = {}
@@ -15,3 +15,20 @@ def signup(request):
         "title" : "Signup",
     })
     return render(request, "registration/signup.html", context)
+
+def signin(request):
+    context = {}
+    form = AuthenticationForm(request, data=request.POST)
+    if request.method == "POST":
+        if form.is_valid():
+            user = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=user, password=password)
+            if user is not None:
+                return redirect("home")
+            
+        context.update({
+            "form": form,
+            "tittle": "Signin"
+        })
+    return render(request, "registration/login.html", context)

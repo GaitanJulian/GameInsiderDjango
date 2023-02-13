d0aa7dd49781677d7647e447fb8cb80e36ad9c1c
GaitanJulian <juliangaitan_h@hotmail.com>
2023-02-13 10:41:41 -0500

    static files fixed

--- myproject/settings.py
+++ myproject/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "templates")],
+        'DIRS': [os.path.join(BASE_DIR,"templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,7 +119,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR, "static/")
+
+STATIC_ROOT = ''
+
+STATICFILES_DIRS = ('static',)
+STATIC_URL = '/static/'
+#STATIC_URL = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

db73b9fbf3e68cec1a6a1e7ca8e7889a0c42d268
GaitanJulian <juliangaitan_h@hotmail.com>
2023-02-15 12:14:16 -0500

    add carousel with dynamic games to home.html template

--- destacado/views.py
+++ destacado/views.py
@@ -5,13 +5,14 @@ from juegos.models import Game
 # Create your views here.
 
 def home(request):
-    featured_posts = FeaturedPost.objects.all()
-    featured_games = Game.objects.all()
-    posts = Post.objects.all()
+    #featured_posts = FeaturedPost.objects.all()
+    games = Game.objects.all()
+    game_groups = [games[i:i+3] for i in range(0, len(games), 3)]
+    #posts = Post.objects.all()
     context = {
-        'featured_post' : featured_posts,
-        'posts' : posts,
-        'featured_games': featured_games,
+        #'featured_post' : featured_posts,
+        #'posts' : posts,
+        'game_groups': game_groups,
     }
 
     return render(request, 'home.html', context)
